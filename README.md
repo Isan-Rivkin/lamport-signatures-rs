239bc1f03e5e68ddadcb1f61c462b44c77367283
Isan-Rivkin <isanrivkin@gmail.com>
2018-11-29 16:24:54 +0200

    DOC: added documentation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # lamport-signatures-rs
 In cryptography, a Lamport signature or Lamport one-time signature scheme is a method for constructing a digital signature. Lamport signatures can be built from any cryptographically secure one-way function; usually a cryptographic hash function is used.  Although the potential development of quantum computers threatens the security of many common forms of cryptography such as RSA, it is believed that Lamport signatures with large hash functions would still be secure in that event. Unfortunately, each Lamport key can only be used to sign a single message. However, combined with hash trees, a single key could be used for many messages, making this a fairly efficient digital signature scheme.
+
+# How to Use 
+Visit the test in lib.rs or the documentation on crates.io.
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,33 @@
+//! In cryptography, a Lamport signature or Lamport one-time signature 
+//! scheme is a method for constructing a digital signature. 
+//! Lamport signatures can be built from any cryptographically 
+//! secure one-way function; usually a cryptographic hash function is used.
+//! Although the potential development of quantum computers 
+//! threatens the security of many common forms of cryptography such as RSA,
+//! it is believed that Lamport signatures with large hash functions
+//! would still be secure in that event. Unfortunately,
+//! each Lamport key can only be used to sign a single message.
+//! However, combined with hash trees,
+//! a single key could be used for many messages, 
+//! making this a fairly efficient digital signature scheme.
+//!
+//! # Use at your own risk.
+//! Example:
+//! ```rust
+//! // generate private key 
+//! let priv_key = lamport_utils::gen_secret_key().unwrap();
+//! // derive public key 
+//! let pub_key = lamport_utils::derive_pub_key(&priv_key);
+//! // create some message 
+//! let msg = "hi elichai2, some secret msg";
+//! // digest the msg 
+//! let msg_digest = lamport_utils::hash(msg.as_bytes());
+//! // sign the digest 
+//! let signature : Vec<[u8;32]> = priv_key.sign(&msg_digest);
+//! // verify signature against public key 
+//! let is_valid = pub_key.verify(&msg_digest, &signature);
+//! ```
+
 extern crate ring;
 extern crate bit;
 pub mod lamport_utils;
